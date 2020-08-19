32f604c24a3941ee167fdc80daec9009b9a7e5cd
Ashwin John Chempolil <chempolil.a@northeastern.edu>
2020-08-19 16:41:41 -0400

    Edited the layout of the web app

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import nltk.corpus
+import nltk
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
 import joblib
 nltk.download('stopwords')
 st.title('Email Spam Classifier')
-punctuation = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+punctuation = ["""!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""]
+    
 @st.cache
 def remove_punct_stopwords(message):
     form_str = [char for char in message if char not in punctuation]
@@ -17,15 +18,22 @@ def remove_punct_stopwords(message):
     words_stop.append('subject')
     form_str_stop = [word for word in form_str_join.split() if word.lower() not in words_stop]
     return form_str_stop
+
+
 spam_model = joblib.load('M_NB_spam_model.joblib')
 vectorizer = joblib.load('CountVectorizer.joblib')
-inp_text = st.text_area('Paste the email to determine whether it is Spam or Ham',height=122)
+inp_text = st.text_area('Paste the email to determine whether it is Spam or Ham',height=200)
+
 vectorised_text = vectorizer.transform([inp_text])
-prediction = spam_model.predict(vectorised_text)
-if prediction == 0:
-    pred = 'Ham'
-else:
-    pred = 'Spam'
+pred = ''
+# add a placeholder
 
-
-st.write(pred)
+def spam_predict(inp_text):
+    prediction = spam_model.predict(inp_text)
+    if prediction == 0:
+        pred = 'Ham'
+    else:
+        pred = 'Spam'
+    return pred
+if st.button('Submit'):
+    st.write('The email you entered is:',spam_predict(vectorised_text))
